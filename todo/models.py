9511cdf9ed99d3c85e78e90de5ebdbfae1fad836
nishanth-kannan <54301294+nishanth-kannan@users.noreply.github.com>
2020-09-27 13:10:07 +0530

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db.models import Model
 
 # Create your models here.
+
+#model for a list of tasks and their completion status
 class List(Model):
     item = models.CharField(max_length = 200)
     completed = models.BooleanField(default = 'False')
