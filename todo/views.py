16ae73b6433ec5b540bf8474feb3b2e9a3e18088
nishanth-kannan <54301294+nishanth-kannan@users.noreply.github.com>
2020-09-27 12:46:25 +0530

    Add files via upload

---
+++ todo/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    if request.method == "POST":
+        form = ListForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all 
+            messages.success(request , ("Task has been added to list!"))
+            return redirect('index') #render(request, "todo/index.html", {'all_items': all_items})
+    else:
+        all_items = List.objects.all
+        return render(request, "todo/index.html", {'all_items': all_items})
+
+def delete(request, list_id):
+    item = List.objects.get(pk= list_id)
+    item.delete()
+    messages.success(request, ("Task has been removed from list!"))
+    return redirect('index')
+        
+def cross_off(request, list_id):
+    item = List.objects.get(pk= list_id)
+    item.completed = True
+    item.save()
+    return redirect('index')
+
+def uncross(request, list_id):
+    item = List.objects.get(pk= list_id)
+    item.completed = False
+    item.save()
+    return redirect('index')
+
+def edit(request, list_id):
+    if request.method == "POST":
+        item = List.objects.get(pk= list_id)
+
+        form = ListForm(request.POST or None, instance= item)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all 
+            messages.success(request, ("Task has been edited successfully!"))
+            return redirect('index')
+        
+    else:
+        item = List.objects.get(pk= list_id)
+        return render(request, 'todo/edit.html', {'item': item})
+
+    
+
+    
+
